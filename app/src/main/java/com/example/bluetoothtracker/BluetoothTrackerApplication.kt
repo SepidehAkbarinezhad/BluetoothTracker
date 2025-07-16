af31ca1039657814913eeed10bce26ed7d570a25
sepidehAkbarinezhad <sepid_ak@yahoo.com>
2025-07-16 19:09:27 +0330

    config hilt

---
+++ app/src/main/java/com/example/bluetoothtracker/BluetoothTrackerApplication.kt
@@ -0,0 +1,7 @@
+package com.example.bluetoothtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BluetoothTrackerApplication : Application()
