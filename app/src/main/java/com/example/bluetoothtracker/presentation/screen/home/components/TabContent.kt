0533915e9b7e4fdf910d7c9058dafbebb5e4aeef
sepideh.akbarinezhad <sepid_ak@yahoo.com>
2025-07-19 17:53:41 +0330

    add list tab

--- app/src/main/java/com/example/bluetoothtracker/presentation/screen/home/components/TabContent.kt
+++ app/src/main/java/com/example/bluetoothtracker/presentation/screen/home/components/TabContent.kt
@@ -1,11 +1,7 @@
 package com.example.bluetoothtracker.presentation.screen.home.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,12 +15,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.bluetoothtracker.presentation.screen.home.HomeAction
 import com.example.bluetoothtracker.presentation.screen.home.HomeState
 import com.example.bluetoothtracker.presentation.utils.printLog
 
 @Composable
-fun DeviceList(
+fun TabContent(
     state: HomeState,
     onAction: (HomeAction) -> Unit,
     modifier: Modifier = Modifier,
@@ -32,22 +29,31 @@ fun DeviceList(
 ) {
     var tabIndex by remember { mutableIntStateOf(0) }
 
+    val list = if (tabIndex == 0) {
+        printLog("isOnline")
+        state.devicesList.filter { it.isOnline }
+    } else {
+        printLog("offline")
+        state.devicesList.filter { !it.isOnline }
+    }
+
+
+
+    printLog("size ${list.size}")
+
     LazyColumn(
         modifier = Modifier
-            .fillMaxSize().background(Color.Black)
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(Color.Black)
             .statusBarsPadding(),
         state = scrollState
     ) {
-        item {
-            HomeTabs(tabIndex = tabIndex, onTabClicked = { index -> tabIndex = index })
-        }
-        printLog("ffffffffff ${state.devicesList}")
 
-        items(items = state.devicesList, key = { it.macAddress }) { device ->
-           /* Box(modifier.fillMaxWidth().height(100.dp)){
-                AppText(text = device.name, color = Color.Black)
-            }*/
-            DeviceListItem(device = device)
+        items(items = list, key = { it.macAddress }) { device ->
+            DeviceListItem(
+                device = device
+            )
         }
     }
 }
